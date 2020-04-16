27e8205305353f64deeb9b04f652a8dd498adb5b
Sinaru Gunawardena <dubdev@lonelyplanet.com>
2020-04-16 23:54:01 +0100

    Move general methods to base class

--- example-notes-app/models/note.js
+++ example-notes-app/models/note.js
@@ -1,32 +1,6 @@
 import DataModel from '/lib/data-model.js'
 
 class Note extends DataModel {
-  get savePath () {
-    return 'http://localhost:3000/note/post'
-  }
-
-  get updatePath () {
-    return 'http://localhost:3000/note/put'
-  }
-
-  get batchPath () {
-    return 'http://localhost:3000/note/batch'
-  }
-
-  get getPath() {
-    return 'http://localhost:3000/note/get';
-  }
-}
-
-Note.all = async () => {
-  const note = new Note()
-  const response = await axios.get(note.batchPath, { params: { action: 'all' } })
-  return response.data.map((item) => new Note(item))
-}
-
-Note.count = async () => {
-  const notes = await Note.all()
-  return notes.length
 }
 
 export default Note
